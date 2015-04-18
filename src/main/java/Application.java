e3d4dd2d000f0e5404b512f48bb48e2bf47a993f
Mikhail Kory <mkory11230@gmail.com>
2015-04-18 16:10:23 -0400

    First commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Mikhail on 4/18/2015.
+ */
+
+public class Application {
+    public static void main(String[] args) {
+
+// She said "Hello!" to me. New line. "I was glad to see her"
+        System.out.println(" \tShe said \"Hello!\" to me. \n.  \t\"I was glad to see her\"" );
+
+    }
+}

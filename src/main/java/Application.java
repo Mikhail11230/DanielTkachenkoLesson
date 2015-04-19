131dbb9f6b21148bfa69a80f057d7b7f63766985
Mikhail Kory <mkory11230@gmail.com>
2015-04-19 02:59:43 -0400

    //Commit with new file SUmOfDiagonal

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,25 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Date;
+
 /**
  * Created by Mikhail on 4/18/2015.
  */
 
 public class Application {
     public static void main(String[] args) {
+        int timer = 10000;
+        long startTime = System.currentTimeMillis();
+        while (timer > 0) {
 
-// She said "Hello!" to me. New line. "I was glad to see her"
-        System.out.println(" \tShe said \"Hello!\" to me. \n.  \t\"I was glad to see her\"" );
+            int[] arr = new int[1000];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (int) (Math.random() * 100);
+            }
+            Arrays.sort(arr);
+            timer--;
 
+        }
+        System.out.println(System.currentTimeMillis() - startTime + "Milliseconds");
     }
 }
